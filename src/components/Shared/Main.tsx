d4e3aa29167a2ca3cc673d3a34f065412b15909f
ÇETİN <cetinnnarasss@gmail.com>
2023-05-20 18:46:23 +0300

    cont

--- src/components/Shared/Main.tsx
+++ src/components/Shared/Main.tsx
@@ -7,17 +7,14 @@ const Main = () => {
       <section className={styles.section}>
         <aside className={styles.asideLeft}>
           <h6>JOIN US</h6>
-          <div className={styles.bigText}>
-            <h1>In-dept, demanding courses</h1>
-          </div>
-          <div className={styles.paragraph}>
-            <p>We know how large objects will act.</p>
-            <p>but things on a small scale</p>
-          </div>
+
+          <h1>In-dept, demanding courses</h1>
+          <p>
+            We know how large objects will act. <br /> but things on a small scale
+          </p>
+
           <div className={styles.btn}>
-            <button typeof="buton" className={styles.leftBtn}>
-              Get Quote Now
-            </button>
+            <button className={styles.leftBtn}>Get Quote Now</button>
             <button className={styles.rightBtn}>Learn More</button>
           </div>
         </aside>
